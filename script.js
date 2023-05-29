273766d5b3d77aae77a38ca083189ca5d0a11630
“nguyen2109” <n21091999@gmail.com>
2023-05-29 13:48:47 +0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-"use strict";
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < showModal.length; i++) {
+  console.log(showModal[i].textContent);
+}
