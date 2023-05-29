ea61f4fef3f4bba8184e8c4efa14fc532319eed9
“nguyen2109” <n21091999@gmail.com>
2023-05-29 15:22:41 +0700

    up

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ const overlay = document.querySelector('.overlay');
 const closeModal = document.querySelector('.close-modal');
 const showModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < showModal.length; i++) {
-  console.log(showModal[i].textContent);
+function closeModalFunc() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 }
+
+function openModalFunc() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', openModalFunc);
+}
+
+closeModal.addEventListener('click', closeModalFunc);
+overlay.addEventListener('click', closeModalFunc);
